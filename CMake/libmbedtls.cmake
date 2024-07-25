f413a5d7ebd1b780184c08e160779256b8dcbae4
JoeBakalor <jmbakalor@gmail.com>
2024-07-25 11:50:00 -0500

    update libmbedtls inlcludes

--- CMake/libmbedtls.cmake
+++ CMake/libmbedtls.cmake
@@ -1,15 +1,17 @@
-set(MBEDTLS_DIR ${CMAKE_CURRENT_SOURCE_DIR}/libraries/3rdparty/mbedtls)
+set(MBEDTLS_DIR /opt/nordic/ncs/v2.6.0/modules/crypto/mbedtls)
+
+set(MBEDTLS_CONFIG_FILE MBEDTLS_DIR/configs/config-no-entropy.h)
 
 set(MBEDTLS_SRC_CRYPTO
     ${MBEDTLS_DIR}/library/aes.c
     ${MBEDTLS_DIR}/library/aesni.c
-    ${MBEDTLS_DIR}/library/arc4.c
+    # ${MBEDTLS_DIR}/library/arc4.c
     ${MBEDTLS_DIR}/library/aria.c
     ${MBEDTLS_DIR}/library/asn1parse.c
     ${MBEDTLS_DIR}/library/asn1write.c
     ${MBEDTLS_DIR}/library/base64.c
     ${MBEDTLS_DIR}/library/bignum.c
-    ${MBEDTLS_DIR}/library/blowfish.c
+    # ${MBEDTLS_DIR}/library/blowfish.c
     ${MBEDTLS_DIR}/library/camellia.c
     ${MBEDTLS_DIR}/library/ccm.c
     ${MBEDTLS_DIR}/library/chacha20.c
@@ -29,12 +31,12 @@ set(MBEDTLS_SRC_CRYPTO
     ${MBEDTLS_DIR}/library/entropy_poll.c
     ${MBEDTLS_DIR}/library/error.c
     ${MBEDTLS_DIR}/library/gcm.c
-    ${MBEDTLS_DIR}/library/havege.c
+    # ${MBEDTLS_DIR}/library/havege.c
     ${MBEDTLS_DIR}/library/hkdf.c
     ${MBEDTLS_DIR}/library/hmac_drbg.c
     ${MBEDTLS_DIR}/library/md.c
-    ${MBEDTLS_DIR}/library/md2.c
-    ${MBEDTLS_DIR}/library/md4.c
+    # ${MBEDTLS_DIR}/library/md2.c
+    # ${MBEDTLS_DIR}/library/md4.c
     ${MBEDTLS_DIR}/library/md5.c
     ${MBEDTLS_DIR}/library/memory_buffer_alloc.c
     ${MBEDTLS_DIR}/library/nist_kw.c
@@ -57,7 +59,7 @@ set(MBEDTLS_SRC_CRYPTO
     ${MBEDTLS_DIR}/library/psa_its_file.c
     ${MBEDTLS_DIR}/library/ripemd160.c
     ${MBEDTLS_DIR}/library/rsa.c
-    ${MBEDTLS_DIR}/library/rsa_internal.c
+    # ${MBEDTLS_DIR}/library/rsa_internal.c
     ${MBEDTLS_DIR}/library/sha1.c
     ${MBEDTLS_DIR}/library/sha256.c
     ${MBEDTLS_DIR}/library/sha512.c
@@ -65,12 +67,12 @@ set(MBEDTLS_SRC_CRYPTO
     ${MBEDTLS_DIR}/library/timing.c
     ${MBEDTLS_DIR}/library/version.c
     ${MBEDTLS_DIR}/library/version_features.c
-    ${MBEDTLS_DIR}/library/xtea.c
+    # ${MBEDTLS_DIR}/library/xtea.c
 )
 
 set(MBEDTLS_SRC_X509
-    ${MBEDTLS_DIR}/library/certs.c
-    ${MBEDTLS_DIR}/library/pkcs11.c
+    # ${MBEDTLS_DIR}/library/certs.c
+    # ${MBEDTLS_DIR}/library/pkcs11.c
     ${MBEDTLS_DIR}/library/x509.c
     ${MBEDTLS_DIR}/library/x509_create.c
     ${MBEDTLS_DIR}/library/x509_crl.c
@@ -85,16 +87,17 @@ set(MBEDTLS_SRC_TLS
     ${MBEDTLS_DIR}/library/net_sockets.c
     ${MBEDTLS_DIR}/library/ssl_cache.c
     ${MBEDTLS_DIR}/library/ssl_ciphersuites.c
-    ${MBEDTLS_DIR}/library/ssl_cli.c
+    # ${MBEDTLS_DIR}/library/ssl_cli.c
     ${MBEDTLS_DIR}/library/ssl_cookie.c
     ${MBEDTLS_DIR}/library/ssl_msg.c
-    ${MBEDTLS_DIR}/library/ssl_srv.c
+    # ${MBEDTLS_DIR}/library/ssl_srv.c
     ${MBEDTLS_DIR}/library/ssl_ticket.c
     ${MBEDTLS_DIR}/library/ssl_tls.c
 )
 
 set(MBEDTLS_INC
     ${MBEDTLS_DIR}/include
+    ${MBEDTLS_DIR}/include/library
 )
 
 # setup mbedcrypto static library
